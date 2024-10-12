96ccf77bf9899d47d47e3309fcee85116e0c6f10
AkianJS <akianzax@gmail.com>
2024-10-11 21:52:12 -0300

    fix: port for vercel

--- src/libs/constants.ts
+++ src/libs/constants.ts
@@ -1,4 +1,1 @@
-export const PORT =
-   process.env.NODE_ENV === 'production'
-      ? 'amalgama-challenge-git-main-akianjs-projects.vercel.app'
-      : 'http://localhost:3000';
+export const PORT = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000';
